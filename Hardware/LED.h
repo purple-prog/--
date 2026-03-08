b5c1f4ad9aceb482b08672ca18e638075ab48011
purple-prog <18130069363@163.com>
2026-03-08 09:25:20 +0800

    LED部分

---
+++ Hardware/LED.h
@@ -0,0 +1,10 @@
+#ifndef __LED_H
+#define __LED_H
+
+void LED_Init(void);
+void LED_Open(void);
+void LED_Close(void);
+void LED_Blink(void);
+
+
+#endif
--- User/main.c
+++ User/main.c
@@ -1,6 +1,8 @@
 #include "stm32f10x.h"                  // Device header
-#include "delay.h"
+#include "Delay.h"
+#include "LED.h"
 int main(void){
+	LED_Init();
 	while(1){
 		
 	}
