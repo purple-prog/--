c58cfc494bc30f429867297c181ce4cba39e0c3d
purple-prog <18130069363@163.com>
2026-03-08 10:03:31 +0800

    按键控制开关

--- User/main.c
+++ User/main.c
@@ -1,9 +1,16 @@
 #include "stm32f10x.h"                  // Device header
 #include "Delay.h"
 #include "LED.h"
+#include "Key.h"
 int main(void){
+	uint8_t KeyNum;
 	LED_Init();
+	Key_Init();
 	while(1){
-		
+		KeyNum=Key_GetNum();
+		if(KeyNum==1)	//按键按下小车启动
+			while(1){
+				
+			}
 	}
 }
