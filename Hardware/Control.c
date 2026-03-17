2a3be68089962cb0e3d69cdd777234f493f60942
purple-prog <18130069363@163.com>
2026-03-17 19:50:09 +0800

    循迹调整

--- Hardware/Control.c
+++ Hardware/Control.c
@@ -16,30 +16,31 @@ uint16_t Left=0;
 uint16_t Right=0;
 
 void Trace_task(void){
-	if(X1==0&&X2==1&&X3==0&&X4==1) Motor_Speed(4000,4000);
-	else if(X1==0&&X2==1&&X3==1&&X4==1) Motor_Speed(0,4300);
-	else if(X1==1&&X2==1&&X3==0&&X4==1) Motor_Speed(4300,0);
-	else if(X1==1&&X2==0&&X3==1&&X4==1) Motor_Speed(-4500,4500);
-	else if(X1==1&&X2==1&&X3==1&&X4==0) Motor_Speed(4500,-4500);
-	
-	if(X3==0&&X4==0)
-	{
-		Motor_Speed(0,0);
-		Delay_ms(500);
-		do
-		{
-			Motor_Speed(5200,-5200);  //原地右转
-		}while(X1==0);
-	}
-	
-	//拐左直角
-	if(X2==0&&X1==0)
-	{
-		Motor_Speed(0,0);  //停车一下
-		Delay_ms(500);
-		do
-		{
-			Motor_Speed(-5200,5200);  //原地左转
-		}while(X3==0);
-	}
+	if(X1==1&&X2==0&&X3==0&&X4==1) Motor_Speed(2000,2000);
+	else if(X1==1&&X2==0&&X3==1&&X4==1) Motor_Speed(2000,1500);
+	else if(X1==1&&X2==1&&X3==0&&X4==1) Motor_Speed(2000,1500);
+	else if (X1==1&&X2==1&&X3==0&&X4==1) Motor_Speed(1500, 2000);
+    else if (X1 == 0 && X2 == 0 && X3 == 1 && X4 == 1) Motor_Speed(2000, 500);
+    else if (X1 == 1 && X2 == 1 && X3 == 0 && X4 == 0) Motor_Speed(500, 2000);
+    else if (X1 == 1 && X2 == 1 && X3 == 1 && X4 == 1) Motor_Speed(0, 0);
+    if (X1 == 0 && X2 == 0) {
+        Motor_Speed(0, 0);
+        Delay_ms(300);
+        do {
+            Motor_Speed(-5200, 5200);
+            Delay_ms(10);
+        } while (X3 == 0);
+        Motor_Speed(0, 0);
+        Delay_ms(200);
+    }
+    if (X3 == 0 && X4 == 0) {
+        Motor_Speed(0, 0);
+        Delay_ms(300);
+        do {
+            Motor_Speed(5200, -5200);
+            Delay_ms(10);
+        } while (X2 == 0);
+        Motor_Speed(0, 0);
+        Delay_ms(200);
+    }
 }
